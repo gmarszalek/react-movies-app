e38e5ed4677ea6d0d96a02c409845fe30ad4cd1f
gmarszalek <grzegorzmarszalek92@gmail.com>
2020-09-23 20:51:50 +0200

    Updates

---
+++ precache-manifest.4ff367b57b544143b8dc341817be6086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ed375e4e1d45362a16e5127154d0ca",
+    "url": "/react-movies-app/index.html"
+  },
+  {
+    "revision": "ac5f556b1ced5da7da8a",
+    "url": "/react-movies-app/static/css/main.1b6993ea.chunk.css"
+  },
+  {
+    "revision": "be9d328ca99a524457ee",
+    "url": "/react-movies-app/static/js/2.9cf651e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies-app/static/js/2.9cf651e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5f556b1ced5da7da8a",
+    "url": "/react-movies-app/static/js/main.13983ff3.chunk.js"
+  },
+  {
+    "revision": "1b54d9008b505fea4c83",
+    "url": "/react-movies-app/static/js/runtime-main.7a5e3778.js"
+  }
+]);
